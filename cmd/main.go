0329af02b13f22bbb8de7a3d35052a8ae733fbe0
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-10 07:51:20 -0300

    feat: insert register

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -22,4 +23,21 @@ func main() {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+
+	InsertUser(client)
+}
+
+func InsertUser(client *mongo.Client) {
+	collection := client.Database("golang").Collection("users")
+	user := bson.D{
+		{"name", "John Doe"},
+		{"age", 29},
+		{"email", "john@go.dev"},
+	}
+	result, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Inserted user with ID:", result.InsertedID)
 }
