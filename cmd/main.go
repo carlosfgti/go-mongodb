c5cc99fa9a117dd083a28a6d713828a0e1f6673b
Carlos Ferreira (perfil 2) <carlos@especializati.com.br>
2024-09-10 07:46:55 -0300

    feat: connect mongodb

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 func main() {
-	print("hello world")
+	clientOptions := options.Client().ApplyURI("mongodb://admin:pass@localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
